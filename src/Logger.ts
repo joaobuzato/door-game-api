8387192abe77b77e471fad8cb88b93d7bd7cc675
Buzato <joaobuzato@gmail.com>
2023-06-05 22:33:29 -0300

    consertando logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -5,7 +5,6 @@ export function log(text: string) {
   const row = `\n${text};`;
   const filePath = path.join(__dirname, "..", "logger.csv");
   fs.appendFile(filePath, row, (err) => {
-    console.log(row);
     if (err) {
       console.error(`ErrorWriting CSV: ${err}`);
     }
