05ad6d5e747591676794331e528a8a556bd3f4bf
Buzato <joaobuzato@gmail.com>
2023-06-05 23:01:02 -0300

    tentando add logs

--- src/Logger.ts
+++ src/Logger.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 
 export function log(text: string) {
   const row = `\n${text};`;
-  const filePath = path.join(__dirname, "..", "logger.csv");
+  const filePath = path.join(__dirname, "..", "logs.csv");
   fs.appendFile(filePath, row, (err) => {
     if (err) {
       console.error(`ErrorWriting CSV: ${err}`);
